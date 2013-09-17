c287a0cf302dab9117a5528bd7786630ce207194
Sergey Bondar <skiv23@yandex.ru>
2013-09-17 21:50:10 +0200

    added parser

---
+++ parser.py
@@ -0,0 +1,9 @@
+ruby_file = open('ruby_test_scripts/ruby_test_1.rb', 'r')
+context = ruby_file.read()
+
+splited_context = context.split() # .split(" ") gives you \n symbols and blah blah
+for element in splited_context:
+	if element == '':
+		splited_context.remove(element)
+for element in splited_context:
+	print element
