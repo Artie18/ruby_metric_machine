4cb83c7537c3d93cf8ad00606c83f571a4e928fe
artie18 <artyomvirusnyak@gmail.com>
2013-11-13 06:08:54 +0300

    Jilba Metric updated:
    	1) get_final now call all functions for metric to work
    	2) regex for all  functions updated

--- jilba_metric.py
+++ jilba_metric.py
@@ -13,28 +13,32 @@ class JilbaMetric(Metric):
             super(JilbaMetric, self).__init__(source_code)
 
     def __find_if_count(self):
-            #self.__if_count += \
-            #    re.findall('(^|$)if(\s+|\()([a-zA-Z][a-zA-Z\d]*)\s*={1,2}\s*([a-zA-Z][a-zA-Z\d]*)(\s+|\))(do|\{)',
-            #                              self._Metric__source_code._SourceCode__file_as_string).__len__()
-            self.__if_count += re.findall('(^|$)if(\s+|\().+(\s+|\))(do|\{)',
+            self.__if_count += re.findall('(^|\n)\s*if(\s+|\().+(\s+|\))(do|\{)',
                                           self._Metric__source_code._SourceCode__file_as_string).__len__()
     def __find_unless_count(self):
-            self.__unless_count += re.findall('(^|$)unless(\s+|\().+(\s+|\))(do|\{)',
-                                          self._Metric__source_code._SourceCode__file_as_string).__len__()
-
-    def __find_while_count(self):
-            self.__while_count += re.findall('(^|$)while(\s+|\().+(\s+|\))(do|\{)',
+            self.__unless_count += re.findall('(^|\n)\s*unless(\s+|\().+(\s+|\))(do|\{)',
                                           self._Metric__source_code._SourceCode__file_as_string).__len__()
     def __find_while_count(self):
-            self.__each_map_count += re.findall('(^|$)\.(each|map)(\s+do|\s*\{)',
+            self.__while_count += re.findall('(^|\n)\s*while(\s+|\().+(\s+|\))(do|\{)',
+                                          self._Metric__source_code._SourceCode__file_as_string).__len__()
+    def __find_each_map_count(self):
+            self.__each_map_count += re.findall('(^|\n)\s*([a-zA-Z][a-zA-Z\d]*)\.(each|map)(\s+do|\s*\{)',
                                             self._Metric__source_code._SourceCode__file_as_string).__len__()
 
-    def __get_final_result(self):
+    def __generate_result(self):
         return {
             "if's": self.__if_count,
             "unlesses": self.__unless_count,
-            "whiles": self.__while_count
+            "whiles": self.__while_count,
+            "Eaches and Maps": self.__each_map_count
         }
+
+    def __get_final_result(self):
+        self.__find_if_count()
+        self.__find_each_map_count()
+        self.__find_unless_count()
+        self.__find_while_count()
+        return self.__generate_result()
 
     def get_metric_result_as_string(self):
         return str(self.__get_final_result())
