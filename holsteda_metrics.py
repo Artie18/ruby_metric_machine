28e1a5d54597c7ec9577fa27f76b80b6d3f73da0
artie18 <artyomvirusnyak@gmail.com>
2013-11-13 05:37:16 +0300

    holsteda metrics updated:
    	1) Added function generate result
    	2) Function count operators updates all operators parametrs

--- holsteda_metrics.py
+++ holsteda_metrics.py
@@ -5,25 +5,37 @@ import re
 
 class HosltedaMetrics(Metric):
 
-    OPERATORS = [ '=','==','+','+=','-=','*=','/=','*',
-                  '/','-','<<','%','**','!=','<','>','>=',
-                  '**=','%=','<=','<==>','===','.eql?','.equal?' ]
+    OPERATORS = [ '===','<==>','==','+=','-=','*=','/=',
+                  '>=','**=','%=','<=','!=','**','=','+','*',
+                  '/','-','<<','%','<','>','.eql?','.equal?' ]
 
-    __total_operators = 0
-    __unique_operators = 0
+    __total_operators           = 0
+    __unique_operators_count    = 0
+    __unique_operators          = []
 
     def __init__(self, source_code):
             super(HosltedaMetrics, self).__init__(source_code)
 
-    def __get_final_result(self):
+    def __generate_result(self):
+        return {
+            "Total operators"   : self.__total_operators        ,
+            "Unique operators"  : self.__unique_operators_count
+        }
+
+    def __count_operators_statistic(self):
+        cleaned_code = self._Metric__source_code._SourceCode__file_as_string
         for operator in self.OPERATORS:
-            operators_count = self.self._Metric__source_code._SourceCode__file_as_string.count(operator)
+            operators_count = cleaned_code.count(operator)
             if operators_count > 0:
-                self.cleaned_code = self.cleaned_code.replace(operator, " ")
+                cleaned_code = cleaned_code.replace(operator, " ")
                 self.__total_operators += operators_count
-                self.__unique_operators.add(operator)
-        self.total_operators_count += self.__total_operators
-        self.unique_operators_count += len(self.__unique_operators)
+                self.__unique_operators.append(operator)
+        self.__unique_operators_count += len(self.__unique_operators)
+
+    def __get_final_result(self):
+        self.__count_operators_statistic()
+#self.__count_operands()
+        return self.__generate_result()
 
     def get_metric_result_as_string(self):
         return str(self.__get_final_result())
