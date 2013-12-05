ac75a4d63979be2eea514cc6fb0b19f46c3024d8
artie18 <artyomvirusnyak@gmail.com>
2013-12-05 22:50:02 +0300

    added rails project compability

--- file_manager.py
+++ file_manager.py
@@ -5,9 +5,36 @@ import os
 class FileManager:
 
     RUBY_FILES_FOLDER = "ruby_test_scripts/"
+    RAILS_PROJECTS = "rails_test_scripts/"
 
     def __init__(self):
         pass
+
+    @classmethod
+    def get_all_rails_projects_from_dir(self):
+        os.chdir(FileManager.RAILS_PROJECTS)
+        result =  [d for d in os.listdir('.') if os.path.isdir(d)]
+        os.chdir('..')
+        return  result
+
+    @classmethod
+    def get_ruby_source_code_from_project(self, projectname):
+        results = ""
+        results = self.__read_files(FileManager.RAILS_PROJECTS + projectname, results)
+        return results
+
+    @classmethod
+    def __read_files(self, dir_name, results):
+        os.chdir(dir_name)
+        for dirpath, dirnames, filenames in os.walk('.'):
+            for dir in dirnames:
+                if dir != "views":
+                    results += self.__read_files(dir, results)
+            filenames = os.listdir('.')
+            for filename in [f for f in filenames if f.endswith(".rb")]:
+                results += (open(os.path.abspath(filename), 'r').read())
+        os.chdir('..')
+        return results
 
     @classmethod
     def get_all_ruby_files_from_dir(self):
@@ -20,7 +47,7 @@ class FileManager:
 
     @classmethod
     def read_file(self, file_name):
-        return open('ruby_test_scripts/ruby_test_1.rb', 'r').read()
+        return open('ruby_test_scripts/' + file_name, 'r').read()
 
 
 
--- main.py
+++ main.py
@@ -15,26 +15,34 @@ ruby_file = ""
 #*** That where we start ***#
 print("Hello everybody")
 print("Lets start analyzing!!!")
+print("Choose between project and ruby")
+
 print("Please choose one of the presents files:")
+print("1) File")
+print("2) Project")
+type_value = int(input("Make a choise"))
+if type_value == 2:
+    input_value = FileManager.get_all_rails_projects_from_dir()
+    for index, file in enumerate(input_value):
+        print(index + 1,")",file)
+    rails_project_name = input_value[int(input("Choose your project name:")) - 1]
+    print("Your file name is", rails_project_name)
+    file_as_a_string = FileManager.get_ruby_source_code_from_project(rails_project_name)
+elif type_value == 1:
+    input_value = FileManager.get_all_ruby_files_from_dir()
+    for index, file in enumerate(input_value):
+        print(index + 1,")",file)
+    ruby_file_name = input_value[int(input("Choose your file:")) - 1]
+    print("Your file name is", ruby_file_name)
+    file_as_a_string = FileManager.read_file(ruby_file_name)
 
-all_ruby_files = FileManager.get_all_ruby_files_from_dir()
-
-for index, file in enumerate(all_ruby_files):
-    print(index + 1,")",file)
-
-ruby_file_name = all_ruby_files[int(input("Choose your file:")) - 1]
-
-print("Your file name is", ruby_file_name)
-
-file_as_a_string = FileManager.read_file('')
-
-source_code = SourceCode(file_as_a_string)
 try:
+    source_code = SourceCode(file_as_a_string)
     if source_code.check_if_valid():
         print("Everything looks great in your source code")
         for index, metric in enumerate(METRICS):
             print(index + 1, metric)
-        current_metric_name = METRICS[int(raw_input("Choose your metric:")) - 1]
+        current_metric_name = METRICS[int(input("Choose your metric:")) - 1]
         metric = MetricsFabric.create_metric(current_metric_name,source_code)
         final_result = metric.get_metric_result_as_string() # common method that is used by all metrics
         print(final_result)
